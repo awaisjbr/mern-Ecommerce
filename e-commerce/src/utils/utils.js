75f0ce775bc48648648a90034606c5e2953e74e8
awaisjbr <awaisjbr@gmail.com>
2025-05-18 10:14:47 +0500

    18-05-2025

--- e-commerce/src/utils/utils.js
+++ e-commerce/src/utils/utils.js
@@ -1,6 +1,6 @@
 import axios from "axios"
 export const axiosInstance = axios.create({
-    baseURL: "http://localhost:4000/api",
+    baseURL: "https://mern-ecommerce-backend-iota.vercel.app/",
     withCredentials: true
 });
 
