352043054ce2f1b0a2c012e3fb3c5478f7a8352b
awaisjbr <awaisjbr@gmail.com>
2025-06-12 11:22:07 +0500

    12-06-2025 2

--- server/src/utils/generateTokenAndCookie.js
+++ server/src/utils/generateTokenAndCookie.js
@@ -5,8 +5,8 @@ export const generateTokenAndCookies = (user, res) => {
     const token = jwt.sign({userId: user._id}, process.env.SECRET_KEY, {expiresIn: "12h"});
     res.cookie("token", token, {
         httpOnly: true,
-        secure: process.env.NODE_ENV === "production",
-        sameSite: process.env.NODE_ENV === "production" ? "none" : "strict",
+        secure: process.env.NODE_ENV === "production" ? false : true,
+        sameSite: process.env.NODE_ENV === "production" ? "None" : "strict",
         maxAge: 12 * 60 * 60 * 1000,
     });
     return token;
