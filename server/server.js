932ef40722b804259dc09fd0a9173273fc6d7eec
awaisjbr <awaisjbr@gmail.com>
2025-06-13 08:42:56 +0500

    13-04-2025

--- server/server.js
+++ server/server.js
@@ -11,7 +11,7 @@ const port = process.env.PORT || 5000;
 const app = express();
 
 app.use(cors({
-  origin: ["https://mern-ecommerce-frontend-lovat.vercel.app", "http://localhost:3000"],
+  origin: 'https://mern-ecommerce-frontend-lovat.vercel.app',
   credentials: true,
 }));
 app.use(express.json({limit: "5mb"}));
